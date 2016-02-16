6bd1f751622a0d64134c40c2bd1b8520a1abe4fb
Tanachat Nilanon <tnilanon@users.noreply.github.com>
2016-02-16 03:02:13 -0800

    Add copy_to_home_directory.sh for Windows

---
+++ copy_to_home_directory.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+case ${OSTYPE} in
+	msys*|cygwin*)
+		find ${PWD} -maxdepth 1 -iname ".*" -type f | xargs -I % cp % ${HOME} ;;
+	*)
+		echo "This script should only be run on Windows" ;;
+esac
+
+
