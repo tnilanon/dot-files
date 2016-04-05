7b4f2dd7f1ea10d6b03f31d54557dd4bea55a6c1
Tanachat Nilanon <tnilanon@users.noreply.github.com>
2016-04-05 14:16:53 -0700

    Make copying verbose

--- copy_to_home_directory.sh
+++ copy_to_home_directory.sh
@@ -2,7 +2,7 @@
 
 case ${OSTYPE} in
 	msys*|cygwin*)
-		find ${PWD} -maxdepth 1 -iname ".*" -type f | xargs -I % cp % ${HOME} ;;
+		find ${PWD} -maxdepth 1 -iname ".*" -type f | xargs -I % cp -v % ${HOME} ;;
 	*)
 		echo "This script should only be run on Windows" ;;
 esac
