da24a5e9b8b51c9840d79034818e7cc3c87555f9
Tanachat Nilanon <tnilanon@users.noreply.github.com>
2016-04-25 10:59:28 -0700

    Fixes in link_to_home_directory.sh
    Make moving existing .bashrc and .profile verbose
    Better logic in linking new files

--- link_to_home_directory.sh
+++ link_to_home_directory.sh
@@ -2,15 +2,28 @@
 
 temp="${HOME}/.bashrc"
 if [[ -f ${temp} && ! -L ${temp} ]]; then
-	mv ${temp} "${HOME}/.bashrc.local"
+	mv -v ${temp} "${HOME}/.bashrc.local"
 fi
 temp="${HOME}/.profile"
 if [[ -f ${temp} && ! -L ${temp} ]]; then
-	mv ${temp} "${HOME}/.profile.local"
+	mv -v ${temp} "${HOME}/.profile.local"
 fi
 unset temp
 
-find ${PWD} -maxdepth 1 -iname ".*" -type f | xargs -I % ln -sv % ${HOME}
-ln -sv ${PWD}/.ssh/config ${HOME}/.ssh/config
+#find ${PWD} -maxdepth 1 -iname ".*" -type f | xargs -I % ln -sv % ${HOME}
+#ln -sv ${PWD}/.ssh/config ${HOME}/.ssh/config
+
+file_list=$(find ${PWD} -maxdepth 1 -iname ".*" -type f | sort)
+file_list="${file_list} ${PWD}/.ssh/config"
+
+for file in ${file_list}
+do
+	file_base=${file#${PWD}/}
+	if [[ -L ${HOME}/${file_base} ]]; then
+		echo ${file_base} is already linked
+	else
+		ln -sv ${PWD}/${file_base} ${HOME}/${file_base}
+	fi
+done
 
 
