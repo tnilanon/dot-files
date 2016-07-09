c4aa22bc2336182c927f3a082f9e4c6cfc26e001
Tanachat Nilanon <tnilanon@users.noreply.github.com>
2016-07-09 15:06:34 -0700

    Add a new script to update dot files

--- copy_to_home_directory.sh
+++ copy_to_home_directory.sh
@@ -4,6 +4,7 @@ case ${OSTYPE} in
 	msys*|cygwin*)
 		find ${PWD} -maxdepth 1 -iname ".*" -type f | xargs -I % cp -v % ${HOME}
 		cp -v ${PWD}/.ssh/config ${HOME}/.ssh/config
+		cp -v ${PWD}/update_dot_files.sh ${HOME}/update_dot_files.sh
 		;;
 	*)
 		echo "This script should only be run on Windows"
---
+++ update_dot_files.sh
@@ -0,0 +1,15 @@
+# update_dot_files.sh
+
+cd "${HOME}/codes/dot-files"
+git update
+case ${OSTYPE} in
+	darwin*)  './link_to_home_directory.sh' ;;
+	linux*)   './link_to_home_directory.sh' ;;
+	msys*)    './copy_to_home_directory.sh' ;;
+	cygwin*)  './copy_to_home_directory.sh' ;;
+	bsd*)     './link_to_home_directory.sh' ;;
+	solaris*) './link_to_home_directory.sh' ;;
+	*)        echo "Can't recognize \${OSTYPE} ${OSTYPE} for .profile" ;;
+esac
+
+
