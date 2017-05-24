c354d3efb6250d2f328770f817ed6aff5f509640
Tanachat Nilanon <tnilanon@users.noreply.github.com>
2017-05-24 11:32:45 -0700

    Quick fix to link & copy jupyterthemes plot style

--- copy_to_home_directory.sh
+++ copy_to_home_directory.sh
@@ -5,6 +5,7 @@ case ${OSTYPE} in
 		find ${PWD} -maxdepth 1 -iname ".*" -type f | xargs -I % cp -v % ${HOME}
 		cp -v ${PWD}/.ssh/config ${HOME}/.ssh/config
 		cp -v ${PWD}/update_dot_files.sh ${HOME}/update_dot_files.sh
+		cp -v ${PWD}/.ipython/profile_default/startup/01-jupyterthemes-plot-style.ipy ${HOME}/.ipython/profile_default/startup/01-jupyterthemes-plot-style.ipy
 		;;
 	*)
 		echo "This script should only be run on Windows"
