d27fdfb862e46477cdb8b7efc23d1b64b5237a19
Tanachat Nilanon <tnilanon@users.noreply.github.com>
2017-09-05 18:08:07 -0700

    Fix script to ignore empty crontab

--- install_append_crontab.sh
+++ install_append_crontab.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-(crontab -l; echo "@reboot	sleep 15s; screen -d -m -S nb /home/gor/start_notebook_server.sh") | crontab -
+current_crontab="""$(crontab -l 2>/dev/null)
+@reboot	sleep 15s; screen -d -m -S nb /home/gor/start_notebook_server.sh
+"""
+
+echo ${current_crontab} | crontab -
 
 
