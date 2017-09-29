4b450ecb27c5a0332cc28cf9182201f95d64d508
Tanachat Nilanon <tnilanon@users.noreply.github.com>
2017-09-29 14:03:20 -0700

    Add pip config

--- copy_to_home_directory.sh
+++ copy_to_home_directory.sh
@@ -3,6 +3,7 @@
 case ${OSTYPE} in
 	msys*|cygwin*)
 		find ${PWD} -maxdepth 1 -iname ".*" -type f | xargs -I % cp -v % ${HOME}
+		cp -v ${PWD}/.config/pip/pip.conf ${HOME}/.config/pip/pip.conf
 		cp -v ${PWD}/.ipython/profile_default/startup/01-jupyterthemes-plot-style.ipy ${HOME}/.ipython/profile_default/startup/01-jupyterthemes-plot-style.ipy
 		cp -v ${PWD}/.ssh/config ${HOME}/.ssh/config
 		cp -v ${PWD}/start_notebook_server.sh ${HOME}/start_notebook_server.sh
--- link_to_home_directory.sh
+++ link_to_home_directory.sh
@@ -14,7 +14,7 @@ unset temp
 #ln -sv ${PWD}/.ssh/config ${HOME}/.ssh/config
 
 file_list=$(find ${PWD} -maxdepth 1 -iname ".*" -type f | sort)
-file_list="${file_list} ${PWD}/.ipython/profile_default/startup/01-jupyterthemes-plot-style.ipy ${PWD}/.ssh/config ${PWD}/start_notebook_server.sh ${PWD}/update_dot_files.sh"
+file_list="${file_list} ${PWD}/.config/pip/pip.conf ${PWD}/.ipython/profile_default/startup/01-jupyterthemes-plot-style.ipy ${PWD}/.ssh/config ${PWD}/start_notebook_server.sh ${PWD}/update_dot_files.sh"
 case ${OSTYPE} in
 	linux*)	file_list="${file_list} ${PWD}/ubuntu-upgrade-security-updates.sh" ;;
 	*)			echo 'Skipping ubuntu-upgrade-security-updates.sh as it is specific to Ubuntu' ;;
