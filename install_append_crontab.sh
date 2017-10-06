758bdf2abe684d0348b4e7f96b1a39c72c473b58
Tanachat Nilanon <tnilanon@users.noreply.github.com>
2017-10-05 19:52:56 -0700

    Fix Jupyter notebook server to start properly

--- install_append_crontab.sh
+++ install_append_crontab.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 current_crontab="""$(crontab -l 2>/dev/null)
-@reboot	sleep 15s; screen -d -m -S nb /home/gor/start_notebook_server.sh
+@reboot	sleep 15s; /home/gor/start_notebook_server.sh
 """
 
-echo ${current_crontab} | crontab -
+echo "${current_crontab}" | crontab -
 
 
--- start_notebook_server.sh
+++ start_notebook_server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source ${HOME}/_conda_/bin/activate nb
-jupyter notebook
+screen -S nb -d -m bash -c "source ${HOME}/_conda_/bin/activate nb; jupyter notebook"
 
 
