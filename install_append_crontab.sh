d1220ff07fb90615375cde6dcced041329c564d6
Tanachat Nilanon <tnilanon@users.noreply.github.com>
2017-10-19 16:14:16 -0700

    Fix install_append_crontab to handle empty crontab

--- install_append_crontab.sh
+++ install_append_crontab.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-current_crontab="""$(crontab -l 2>/dev/null)
-@reboot	sleep 15s; /home/gor/start_notebook_server.sh
-"""
+NEWLINE=$'\n'
 
-echo "${current_crontab}" | crontab -
+old_crontab=$(crontab -l 2>/dev/null)
+if [[ ${old_crontab} ]]; then
+	old_crontab="${old_crontab}${NEWLINE}"
+fi
+
+new_crontab="""${old_crontab}@reboot	sleep 15s; /home/gor/start_notebook_server.sh"""
+
+echo "${new_crontab}" | crontab -
 
 
